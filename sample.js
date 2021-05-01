ae29d25e8fe8293b4f7fe91a91a35d635e6d5366
anfal7997 <anfalsha.14@gmail.com>
2021-05-01 16:45:19 +0530

    Committed

---
+++ sample.js
@@ -0,0 +1,34 @@
+const firstName = [];
+const lastName = [];
+const email = [];
+const phoneNum = [];
+const password = [];
+
+const fname = document.getElementById("Fname");
+const lname = document.getElementById("Lname");
+const Email = document.getElementById("email");
+const phn = document.getElementById("phn");
+const pass = document.getElementById("pass");
+
+function add() {
+  firstName.push(fname.value);
+  lastName.push(lname.value);
+  email.push(Email.value);
+  phoneNum.push(phn.value);
+  password.push(pass.value);
+
+  clearAnd();
+}
+
+function clearAnd() {
+  fname.value = "";
+  lname.value = "";
+  Email.value = "";
+  phn.value = "";
+  pass.value = "";
+
+  document.getElementById("display").innerHTML = firstName;
+  document.getElementById("display1").innerHTML = lastName;
+  document.getElementById("display2").innerHTML = email;
+  document.getElementById("display3").innerHTML = phoneNum;
+}
